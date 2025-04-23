76afe1f3a6750a613b39f993ae0165a14e5464a5
GUILHERME VILELA <107810558+guizaolopes@users.noreply.github.com>
2025-04-22 22:19:06 -0300

    Update iluminacao.ino

--- iluminacao.ino
+++ iluminacao.ino
@@ -46,3 +46,37 @@ void handleRoot() {
   htmlPage.replace("%STATUS%", estadoRele ? "LIGADA" : "DESLIGADA");
   server.send(200, "text/html", htmlPage);
 }
+
+// alternando o estado da lampada
+void handleToggle() {
+  estadoRele = !estadoRele;
+  digitalWrite(relePin, estadoRele ? HIGH : LOW);
+  handleRoot();
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(relePin, OUTPUT);
+  digitalWrite(relePin, LOW);
+
+  WiFi.begin(ssid, password);
+  Serial.print("Conectando-se ao Wi-Fi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nWi-Fi conectado.");
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", handleRoot);
+  server.on("/toggle", handleToggle);
+  server.begin();
+  Serial.println("Servidor iniciado.");
+}
+
+void loop() {
+  server.handleClient();
+}
+
